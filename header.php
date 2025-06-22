208f0aefd61a3b2d7c7b5dcf323f8ed36f862dae
citrafardiani-24 <148729858+citrafardiani-24@users.noreply.github.com>
2025-06-22 11:01:07 +0700

    header.php

--- header.php
+++ header.php
@@ -4,6 +4,9 @@
   <meta charset="UTF-8">
   <title>Cashiary - Transaksi</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body></body>
